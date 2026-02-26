36bc0e969f3deace0b2fc0bd60a55fddcb94231b
danh phuc <phucdh2@zigexn.vn>
2026-02-26 15:27:35 +0700

    Add GitHub Pages deployment workflow and set base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/buy-house/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
